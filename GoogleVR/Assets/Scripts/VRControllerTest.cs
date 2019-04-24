5f890932cd34df065e589412262eae7be8c19619
AftabBigO <aftab.pilover@gmail.com>
2019-04-24 18:17:43 +0600

    First Commit from local repo

---
+++ GoogleVR/Assets/Scripts/VRControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRControllerTest : MonoBehaviour
+{
+    [SerializeField] private Transform cubeTransform;
+    [SerializeField] private float speed;
+    private bool canRotate;
+
+    private void Update()
+    {
+        if(canRotate)
+        {
+            RotateCube();
+        }
+    }
+
+    private void RotateCube()
+    {
+        cubeTransform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    public void TriggerRotate()
+    {
+        canRotate =(!canRotate);
+    }       
+}
